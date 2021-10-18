455e575d2c78c63d377af2ae2ce4beee232c1561
Võ Trần Minh Quân <vtmquan0909@gmail.com>
2021-10-18 16:22:56 +0700

    first commit

---
+++ src/main/java/hcmute/manage/club/uteclubs/UteClubsApplication.java
@@ -0,0 +1,13 @@
+package hcmute.manage.club.uteclubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UteClubsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UteClubsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hcmute/manage/club/uteclubs/UteClubsApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.manage.club.uteclubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UteClubsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
