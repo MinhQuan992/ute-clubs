124575f4fedcc014ee64b475b8348cd76689115c
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2022-06-11 17:41:15 +0700

    fix: add more constructors in UserResponse class

--- src/main/java/hcmute/manage/club/uteclubs/framework/dto/user/UserResponse.java
+++ src/main/java/hcmute/manage/club/uteclubs/framework/dto/user/UserResponse.java
@@ -1,12 +1,16 @@
 package hcmute.manage.club.uteclubs.framework.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Date;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
     private Long userId;
     private String fullName;
